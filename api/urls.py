f7dcec5401e6b4361ccbd958fddef0cd6e024c5c
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2024-03-28 13:02:35 -0700

    move docs to /docs/

--- api/urls.py
+++ api/urls.py
@@ -6,25 +6,6 @@ from django.views.generic import TemplateView
 from rest_framework.routers import SimpleRouter
 
 
-from django.urls import path, re_path
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version="v1",
-        description="API documentation for my project",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@myproject.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-
 class OptionalSlashRouter(SimpleRouter):
     """OptionalSlashRouter"""
 
@@ -36,16 +17,4 @@ class OptionalSlashRouter(SimpleRouter):
 
 urlpatterns = [
     path("", include("api.dawnet.urls")),
-    # Swagger
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
--- app/urls.py
+++ app/urls.py
@@ -26,6 +26,25 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+
+from django.urls import path, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="API documentation for my project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myproject.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 # pylint: disable=invalid-name
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -48,6 +67,22 @@ if settings.APP_NAME == "web":
 elif settings.APP_NAME == "api":
     urlpatterns += [
         path("api/", include("api.urls")),
+        # Swagger
+        re_path(
+            r"^docs(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        path(
+            "docs/swagger/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "docs/redoc/",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
     ]
 
 if settings.DEBUG:
