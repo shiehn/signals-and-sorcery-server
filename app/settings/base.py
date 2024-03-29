fbb214ae1296780c7398c5563f033b45d60006b5
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2024-03-29 15:07:06 -0700

    conditional static url

--- app/settings/base.py
+++ app/settings/base.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 
+from django.conf import settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -180,7 +182,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = "/static/"
+if settings.APP_NAME == "web":
+    STATIC_URL = "/cms/static/"
+elif settings.APP_NAME == "api":
+    STATIC_URL = "/api/static/"
+else:
+    STATIC_URL = "/static/"
+
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
